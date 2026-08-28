16dd6392386015080df09635c48161b125e448da
NullCode1337 <contact.nullcode@proton.me>
2026-08-28 19:56:51 +0600

    oplus: tp: synaptics_hbp: Report double tap as KEY_WAKEUP

--- oplus/kernel/touchpanel/synaptics_hbp/syna_tcm2.c
+++ oplus/kernel/touchpanel/synaptics_hbp/syna_tcm2.c
@@ -835,6 +835,11 @@ static void syna_dev_report_input_events(struct syna_tcm *tcm)
 					touchpanel_event_call_notifier(EVENT_ACTION_UNDER_WATER, (void *)&tcm->under_water);
 					syna_send_signal(tcm, SIG_UNDER_WATER);
 				}
+			} else if (touch_data->gesture_id == DTAP_DETECT) {
+				input_report_key(input_dev, KEY_WAKEUP, 1);
+				input_sync(input_dev);
+				input_report_key(input_dev, KEY_WAKEUP, 0);
+				input_sync(input_dev);
 			} else if (touch_data->gesture_id == ON_WATER) {
 				if(tcm->under_water){
 					tcm->under_water = FALSE;
@@ -1024,6 +1029,8 @@ static int syna_dev_create_input_device(struct syna_tcm *tcm)
 
 	set_bit(KEY_SLEEP, input_dev->keybit);
 #ifdef ENABLE_WAKEUP_GESTURE
+	set_bit(KEY_WAKEUP, input_dev->keybit);
+	input_set_capability(input_dev, EV_KEY, KEY_WAKEUP);
 	set_bit(KEY_F4, input_dev->keybit);
 	input_set_capability(input_dev, EV_KEY, KEY_F4);
 	set_bit(KEY_UNDER_WATER, input_dev->keybit);
